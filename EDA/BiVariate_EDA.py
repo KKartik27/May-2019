3e05dfd768349141827d629f8e2753955cd6e600
Sreeni Jilla <37336941+dssreeni@users.noreply.github.com>
2019-07-02 09:49:41 +0530

    Update BiVariate_EDA.py

--- EDA/BiVariate_EDA.py
+++ EDA/BiVariate_EDA.py
@@ -2,6 +2,8 @@ import os
 import pandas as pd
 import seaborn as sns
 
+#returns current working directory
+os.getcwd()
 #changes working directory
 os.chdir("D:/Data Science/Data")
 
@@ -12,9 +14,9 @@ titanic_train.shape
 titanic_train.info()
 
 #explore bivariate relationships: categorical vs categorical 
-pd.crosstab(index=titanic_train['Survived'], columns=titanic_train['Pclass'])
+pd.crosstab(index=titanic_train['Survived'], columns=titanic_train['Sex'])
 #Crosstab cab be extended to multiple columsn as well
-pd.crosstab(index=titanic_train['Survived'], columns=[titanic_train['Pclass'], titanic_train['Embarked']])
+pd.crosstab(index=titanic_train['Survived'], columns=[titanic_train['Pclass'], titanic_train['Embarked'], titanic_train['Sex']])
 
 pd.crosstab(index=titanic_train['Survived'], columns=titanic_train['Pclass'])
 
@@ -30,14 +32,13 @@ sns.factorplot(x="Embarked", hue="Survived", data=titanic_train, kind="count", s
 
 #explore bivariate relationships: categorical vs continuous 
 #kind="box", 
-#Factor plot does not make scence on continuous columns
 sns.factorplot(x="Fare", hue="Survived", data=titanic_train, kind="count", size=6)
 sns.factorplot(x="Fare", row="Survived", data=titanic_train, kind="count", size=6)
 #.map is a inline function like a for loop
 #Survived Vs Fare
-sns.FacetGrid(titanic_train, row="Pclass",size=8).map(sns.kdeplot, "Fare").add_legend()
+sns.FacetGrid(titanic_train, row="Survived",size=8).map(sns.kdeplot, "Fare").add_legend()
 sns.FacetGrid(titanic_train, row="Survived",size=8).map(sns.boxplot, "Fare").add_legend()
 sns.FacetGrid(titanic_train, row="Survived",size=8).map(sns.distplot, "Fare").add_legend()
 
 #explore bivariate relationships: continuous vs continuous 
-sns.jointplot(x="Age", y="Fare", data=titanic_train)
+sns.jointplot(x="SibSp", y="Parch", data=titanic_train)
